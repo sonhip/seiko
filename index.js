901756b647c3e901999497f54e42c579980636e4
SonHip <sonhipkmhd@gmail.com>
2024-05-11 22:47:38 +0700

    add res header and footer

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function toggleSidebar() {
   const sidebar = document.getElementById("mySidebar");
   const body = document.body;
+  const icon = document.querySelector(".toggle-icon");
 
   if (sidebar.style.display === "none") {
     sidebar.style.display = "block";
     body.classList.add("body-no-scroll");
+    icon.classList.remove("bi-list");
+    icon.classList.add("bi-x");
   } else {
     sidebar.style.display = "none";
     body.classList.remove("body-no-scroll");
+    icon.classList.remove("bi-x");
+    icon.classList.add("bi-list");
   }
 }
